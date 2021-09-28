694068d788efd0132cc34ecf4f57e76f01bcc952
jqsheng <jqsheng@trip.com>
2021-09-29 01:49:27 +0800

    Initial Commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,21 @@
+import { defineUserConfig } from 'vuepress';
+import type { DefaultThemeOptions } from 'vuepress';
+import sidebar from './config/sidebar';
+import navbar from './config/nav';
+
+export default defineUserConfig<DefaultThemeOptions>({
+  title: 'BFE.LESSONS',
+  description: 'Just play around',
+  themeConfig: {
+    logo: './assets/logo.png',
+    smoothScroll: true,
+    navbar,
+    sidebar,
+    lastUpdated: true,
+    repo: "https://github.com/zpfz/vuepress-creator",
+  },
+  plugins: [
+    ['@vuepress/search', { searchMaxSuggestions: 10 }],
+    // ['@vuepress/docsearch', { apiKey: '<API_KEY>', indexName: '<INDEX_NAME>' }]
+  ]
+});
---
+++ zen.md
@@ -0,0 +1,10 @@
+# bfe.lessons
+
+steven7sheng补课的文档站点
+
+## MISC VuePress搭建步骤
+1. 安装脚手架 `yarn global add vuepress-creator`
+2. 升级模板命令 `vuepress-creator upgrade -t`
+3. 建立项目 `vuepress-creator init dir`
+4. 升级vuepress@next
+5. 调色板不再默认支持需要`npm i -D @vuepress/plugin-palette@next`
